144f483b29fe85ae49d64b9cb2cced951315f938
sora <sorasky0126@outlook.jp>
2026-08-10 19:40:28 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-let hamburger = document.getErementById()
+let hamburger = document.getErementById("js-hamburger");
+let menu = document.getElementById("js-menu");
+
+hamburger.addEventListener(
+  "click", () =>
+    {
+      hamburger.classList.toggle("active");
+      menu.classList.toggle("active");
+    }
+);
