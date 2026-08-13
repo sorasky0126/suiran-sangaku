d291e1a54d41b97ba9dc8c1d21f830a1a5a63b78
sora <sorasky0126@outlook.jp>
2026-08-13 12:52:31 +0900

    Refactor loadHeader and loadFooter functions

--- component/js/main.js
+++ component/js/main.js
@@ -1,57 +1,53 @@
-function loadHeader(){
-  fetch('/component/html/header.html')
-    .then(response => {
-      if(!response.ok) throw new Error('header.htmlの読み込みに失敗しました。');
-      return response.text();
-      
-    })
-    .then(html => {
-      document.getElementById('header').innerHTML = html;
-    })
-    .catch(error => {
-      console.error('header.htmlの読み込みに失敗しました。', error);
-    }); 
-}
-
-function loadFooter(){
-  fetch('/component/html/footer.html')
-    .then(response => {
-      if(!response.ok) throw new Error('footer.htmlの読み込みに失敗しました。');
+function loadHeader() {
+  fetch("/component/html/header.html")
+    .then((response) => {
+      if (!response.ok)
+        throw new Error("header.htmlの読み込みに失敗しました。");
       return response.text();
     })
-    .then(html => {
-      document.getElementById('footer').innerHTML = html;
+    .then((html) => {
+      document.getElementById("header").innerHTML = html;
       reset();
     })
-    .catch(error => {
-      console.error('footer.htmlの読み込みに失敗しました。', error);
+    .catch((error) => {
+      console.error("header.htmlの読み込みに失敗しました。", error);
     });
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+function loadFooter() {
+  fetch("/component/html/footer.html")
+    .then((response) => {
+      if (!response.ok)
+        throw new Error("footer.htmlの読み込みに失敗しました。");
+      return response.text();
+    })
+    .then((html) => {
+      document.getElementById("footer").innerHTML = html;
+    })
+    .catch((error) => {
+      console.error("footer.htmlの読み込みに失敗しました。", error);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
   loadHeader();
   loadFooter();
 });
 
 //ここで、ハンバーガーメニューと、その他諸々のresetを関数にしてあとで呼び出す
-function reset(){
-// ハンバーガーメニュー
-let hamburger = document.getElementById("js-hamburger");
-let menu = document.getElementById("js-menu");
+function reset() {
+  // ハンバーガーメニュー
+  let hamburger = document.getElementById("js-hamburger");
+  let menu = document.getElementById("js-menu");
 
-hamburger.addEventListener(
-  "click", () =>
-    {
-      hamburger.classList.toggle("active");
-      menu.classList.toggle("active");
-    }
-);
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
+  });
 
-// ページ読み込み時に最初の画面高さを取得してpx固定する
-window.addEventListener('DOMContentLoaded', () => {
-  const faceImg = document.querySelector('.face-img');
+  // 最初の画面高さを取得してpx固定する
+  const faceImg = document.querySelector(".face-img");
   if (faceImg) {
     faceImg.style.height = `${window.innerHeight}px`;
   }
-});
-};
+}
