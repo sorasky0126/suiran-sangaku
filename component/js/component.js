491dc0822739d653fd08e1c96c38a0fbf440f1de
sora <sorasky0126@outlook.jp>
2026-08-13 09:47:34 +0900

    Update component.js

--- component/js/component.js
+++ component/js/component.js
@@ -9,6 +9,18 @@ function loadHeader(){
     })
     .catch(error => {
       console.error('header.htmlの読み込みに失敗しました。', error);
-    });
-    
+    }); 
 }
+
+function loadFooter(){
+  fetch('/component/html/footer.html')
+    .then(response => {
+      if(!response.ok) throw new Error('footer.htmlの読み込みに失敗しました。');
+      return responce.text();
+    })
+    .then(html => {
+      document.getElementById('footer').innerHTML = html;
+    })
+    .catch(error => {
+      console.error('footer.htmlの読み込みに失敗しました。', error);
+    });
