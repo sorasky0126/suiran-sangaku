1c83b5075c7a4a783c94d2230541f1e78a422293
sora <sorasky0126@outlook.jp>
2026-08-13 11:29:32 +0900

    Add hamburger menu functionality and height adjustment

--- component/js/main.js
+++ component/js/main.js
@@ -29,3 +29,23 @@ document.addEventListener('DOMContentLoaded', () => {
   loadHeader();
   loadFooter();
 });
+
+// ハンバーガーメニュー
+let hamburger = document.getElementById("js-hamburger");
+let menu = document.getElementById("js-menu");
+
+hamburger.addEventListener(
+  "click", () =>
+    {
+      hamburger.classList.toggle("active");
+      menu.classList.toggle("active");
+    }
+);
+
+// ページ読み込み時に最初の画面高さを取得してpx固定する
+window.addEventListener('DOMContentLoaded', () => {
+  const faceImg = document.querySelector('.face-img');
+  if (faceImg) {
+    faceImg.style.height = `${window.innerHeight}px`;
+  }
+});
